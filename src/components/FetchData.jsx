43265a58537c8cd42a31d96d81f238a025a097e2
Het Siddhapura <het@aarogram.com>
2024-02-06 15:03:19 +0530

    Version-1 deploy ready

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -1,41 +1,65 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const FetchData = ({cat}) => {
-    const [data, setData] = useState("");
-    const FetchData = async () => {
-        await axios.get(
-            cat ? `https://newsapi.org/v2/top-headlines?country=in&category=${cat}&apiKey=80341ce997034709bbdf74f677de70d7` :
-            "https://newsapi.org/v2/top-headlines?country=in&category=bussiness&apiKey=80341ce997034709bbdf74f677de70d7")
-        .then((res) => setData(res.data.articles));
-    };
-    useEffect(() => {
-        FetchData();
-    }, [cat]);
+const FetchData = ({ cat }) => {
+  const [data, setData] = useState("");
+  const FetchData = async () => {
+    await axios
+      .get(
+        cat
+          ? `https://newsapi.org/v2/top-headlines?country=in&category=${cat}&apiKey=80341ce997034709bbdf74f677de70d7`
+          : "https://newsapi.org/v2/top-headlines?country=in&category=bussiness&apiKey=80341ce997034709bbdf74f677de70d7"
+      )
+      .then((res) => setData(res.data.articles));
+  };
+  useEffect(() => {
+    FetchData();
+  }, [cat]);
   return (
     <div className="container my-4">
-        <h3>TOP HEADLINES</h3>
-        <div className="container d-flex justify-content-center align-items-center flex-column my-3" style={{minHeight:"100vh"}}>
-            {data ? data.map((items, index) => (
-                <div key={items.url}> 
-                    <div className="container my-3 p-3" style={{width:"600px", boxShadow:"2px 2px 10px silver", borderRadius:"10px"}}>
-                        <h5 className="my-1">{items.title}</h5>
-                        <div className="d-flex justify-content-center align-items-center">
-                        <img 
-                        src={items.urlToImage} 
-                        alt="Image not found" 
-                        className="img-fluid" 
-                        style={{width:"100%", height:"300px", objectFit:"cover"}}/></div>
-                    <p>{items.content}</p>
-                    <Link to={items.url} target='blank'>View More</Link>
-                    </div>
+      <h3 style={{ textAlign: "center" }}>TOP HEADLINES</h3>
+      <div
+        className="container d-flex justify-content-center align-items-center flex-column my-3"
+        style={{ minHeight: "100vh" }}
+      >
+        {data
+          ? data.map((items, index) => (
+              <div key={items.url}>
+                <div
+                  className="container my-3 p-3"
+                  style={{
+                    width: "600px",
+                    boxShadow: "2px 2px 10px silver",
+                    borderRadius: "10px",
+                  }}
+                >
+                  <h5 className="my-1">{items.title}</h5>
+                  <div className="d-flex justify-content-center align-items-center">
+                    <img
+                      src={items.urlToImage}
+                      alt="Image not found"
+                      className="img-fluid"
+                      style={{
+                        width: "100%",
+                        height: "300px",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>
+                  <p>{items.content}</p>
+                  <Link to={items.url} target="blank">
+                    View More
+                  </Link>
                 </div>
-            )) : "LOADING..."};
-        </div>
+              </div>
+            ))
+          : "LOADING..."}
+        ;
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FetchData
+export default FetchData;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,82 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark text-white">
-  <Link className="navbar-brand text-white" to="/general">NEWS Corner</Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark text-white">
+        <Link
+          className="navbar-brand text-white"
+          to="/general"
+          style={{ marginLeft: "100px" }}
+        >
+          NEWS CORNER
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-  <div className="collapse navbar-collapse text-white" id="navbarSupportedContent">
-    <ul className="navbar-nav m5-auto mb-2 mb-lg-0">
-      <li className="nav-item active text-white">
-        <Link className="nav-link text-white" to="/general">General</Link>
-      </li>
-      <li className="nav-item active text-white">
-        <Link className="nav-link text-white" to="/business">Business</Link>
-      </li>
-      <li className="nav-item active text-white">
-        <Link className="nav-link text-white" to="/entertainment">Entertainment</Link>
-      </li>
-      <li className="nav-item active text-white">
-        <Link className="nav-link text-white" to="/science">Science</Link>
-      </li>
-      <li className="nav-item active text-white">
-        <Link className="nav-link text-white" to="/sports">Sports & Technology</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
+        <div
+          className="collapse navbar-collapse text-white"
+          id="navbarSupportedContent"
+        >
+          <ul
+            className="navbar-nav m5-auto mb-2 mb-lg-0"
+            style={{ paddingLeft: "120px" }}
+          >
+            <li
+              className="nav-item active text-white"
+              style={{ paddingLeft: "20px" }}
+            >
+              <Link className="nav-link text-white" to="/general">
+                General
+              </Link>
+            </li>
+            <li
+              className="nav-item active text-white"
+              style={{ paddingLeft: "20px" }}
+            >
+              <Link className="nav-link text-white" to="/business">
+                Business
+              </Link>
+            </li>
+            <li
+              className="nav-item active text-white"
+              style={{ paddingLeft: "20px" }}
+            >
+              <Link className="nav-link text-white" to="/entertainment">
+                Entertainment
+              </Link>
+            </li>
+            <li
+              className="nav-item active text-white"
+              style={{ paddingLeft: "20px" }}
+            >
+              <Link className="nav-link text-white" to="/science">
+                Science
+              </Link>
+            </li>
+            <li
+              className="nav-item active text-white"
+              style={{ paddingLeft: "20px" }}
+            >
+              <Link className="nav-link text-white" to="/sports">
+                Sports & Technology
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
