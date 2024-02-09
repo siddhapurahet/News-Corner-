267a24acd6d1cc60fbb2392b6dc69a8626484b7c
siddhapurahet <84630752+siddhapurahet@users.noreply.github.com>
2024-02-09 10:48:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         <Navbar />
         <Routes>
           {/* <Route path="/general" element={<FetchData/>}/> */}
-          <Route path="/business" element={<FetchData cat="America"/>}/>
-          <Route path="/entertainment" element={<FetchData cat="Canada"/>}/>
-          <Route path="/general" element={<FetchData cat="United Kingdom"/>}/>
-          <Route path="/science" element={<FetchData cat="India"/>}/>
-          <Route path="/sports" element={<FetchData cat="China"/>}/>
+          <Route path="/business" element={<FetchData cat="in"/>}/>
+          <Route path="/entertainment" element={<FetchData cat="us"/>}/>
+          <Route path="/general" element={<FetchData cat="ca"/>}/>
+          <Route path="/science" element={<FetchData cat="gb"/>}/>
+          <Route path="/sports" element={<FetchData cat="in"/>}/>
         </Routes>
         <Footer />
       </BrowserRouter>
